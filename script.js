9a670af22bfdbe163d951155643e65f0d6b86300
Semi <83414623+isseemii@users.noreply.github.com>
2022-12-23 02:13:51 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+
+  $('div').multilingual([
+    'ko',
+    'en',
+    'punct'
+  ]);
+
+});
+
+// // 대문 인포 클릭 
+// $(function () {
+//   $("#goinfo").click(function(){
+//     $("#information").toggle();
+//     $("#subject").toggle();
+//     $(this).text('home!');
+//   })
+// })
+
+
+
+// // 인터뷰 클릭 
+// // 중복 방지 해야함 
+// $(function () {
+//   $("#nav-yuseon").click(function(){
+//     $("#qna-yuseon").toggle();
+//   })
+// })
+
+// $(function () {
+//   $("#nav-semi").click(function(){
+//     $("#qna-semi").toggle();
+//   })
+// })
+
+
